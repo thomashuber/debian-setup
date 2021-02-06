8ad9a4415ae779a019c3f8715cf7943f5495837d
Thomas Huber <thomas.ch.huber@gmail.com>
2021-02-06 20:09:17 +0100

    check_config.sh Update

--- check_config.sh
+++ check_config.sh
@@ -1,32 +1,55 @@
 #!/usr/bin/env bash
 
-ERRORS_NUMBER=0
+ERRORS_NUMBER_DIR=0
+ERRORS_NUMBER_SYMLINKS=0
+ERRORS_NUMBER_CONFFILES=0
 
+#----------------------------#
+#  Directories 
+#----------------------------#
 if [[ ! -d /etc/apt/apt.conf.d/ ]]; then
-  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
+  ((ERRORS_NUMBER_DIR = ERRORS_NUMBER_DIR + 1))
   echo "/etc/apt/apt.conf.d/ not a directory"
 fi
 if [[ ! -d ~/debian-setup/etc/apt/apt.conf.d/ ]]; then
-  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
+  ((ERRORS_NUMBER_DIR = ERRORS_NUMBER_DIR + 1))
   echo "~/debian-setup/etc/apt/apt.conf.d/ not a directory"
 fi
 
+#----------------------------#
+#  Symlinks
+#----------------------------#
 if [[ ! -h /etc/apt/apt.conf.d/50unattended-upgrades ]]; then
-  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
+  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
   echo "/etc/apt/apt.conf.d/50unattended-upgrades not a symlink" 
 fi
 if [[ ! -h /etc/apt/apt.conf.d/20auto-upgrades ]]; then
-  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
+  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
   echo "/etc/apt/apt.conf.d/20auto-upgrades not a symlink" 
 fi
+if [[ ! -h ~/.bash_aliases ]]; then
+  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
+  echo "~/.bash_aliases not a symlink" 
+fi
 
+#----------------------------#
+#  Configuration files
+#----------------------------#
 if [[ ! -s ~/debian-setup/etc/apt/apt.conf.d/50unattended-upgrades ]]; then
-  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
+  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
   echo "~/debian-setup/etc/apt/apt.conf.d/50unattended-upgrades does not exist or is empty" 
 fi
 if [[ ! -s ~/debian-setup/etc/apt/apt.conf.d/20auto-upgrades ]]; then
-  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
+  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
   echo "~/debian-setup/etc/apt/apt.conf.d/20auto-upgrades  does not exist or is empty" 
 fi
+if [[ ! -s ~/debian-setup/.bash_aliases ]]; then
+  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
+  echo "~/debian-setup/.bash_aliases does not exist or is empty" 
+fi
 
-echo "Number of errors found: $ERRORS_NUMBER"
+if [[ ERRORS_NUMBER_DIR -eq 0 && 
+      ERRORS_NUMBER_SYMLINKS -eq 0 &&
+      ERRORS_NUMBER_CONFFILES -eq 0 ]]; then
+echo "All Tests OK."
+fi
