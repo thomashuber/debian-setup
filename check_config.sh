3c0bdc0a70accab6f5a84fda822a07d2d35caef3
Thomas Huber <thomas.ch.huber@gmail.com>
2021-02-06 12:25:33 +0100

    Bug fix check_config.sh

--- check_config.sh
+++ check_config.sh
@@ -3,29 +3,29 @@
 ERRORS_NUMBER=0
 
 if [[ ! -d /etc/apt/apt.conf.d/ ]]; then
-  ERRORS_NUMBER = ERRORS_NUMBER + 1
+  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
   echo "/etc/apt/apt.conf.d/ not a directory"
 fi
 if [[ ! -d ~/etc/apt/apt.conf.d/ ]]; then
-  ERRORS_NUMBER = ERRORS_NUMBER + 1
+  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
   echo "~/etc/apt/apt.conf.d/ not a directory"
 fi
 
 if [[ ! -h /etc/apt/apt.conf.d/50unattended-upgrades ]]; then
-  ERRORS_NUMBER = ERRORS_NUMBER + 1
+  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
   echo "/etc/apt/apt.conf.d/50unattended-upgrades not a symlink" 
 fi
 if [[ ! -h /etc/apt/apt.conf.d/20auto-upgrades ]]; then
-  ERRORS_NUMBER = ERRORS_NUMBER + 1
+  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
   echo "/etc/apt/apt.conf.d/20auto-upgrades not a symlink" 
 fi
 
 if [[ ! -s ~/etc/apt/apt.conf.d/50unattended-upgrades ]]; then
-  ERRORS_NUMBER = ERRORS_NUMBER + 1
+  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
   echo "~/etc/apt/apt.conf.d/50unattended-upgrades does not exist or is empty" 
 fi
 if [[ ! -s ~/etc/apt/apt.conf.d/20auto-upgrades ]]; then
-  ERRORS_NUMBER = ERRORS_NUMBER + 1
+  ((ERRORS_NUMBER = ERRORS_NUMBER + 1))
   echo "~/etc/apt/apt.conf.d/20auto-upgrades  does not exist or is empty" 
 fi
 
