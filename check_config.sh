b42c113c8dbf37a70551945311f795959db6b3a0
Thomas Huber <thomas.ch.huber@gmail.com>
2021-02-07 14:58:17 +0100

    Update check_config.sh

--- check_config.sh
+++ check_config.sh
@@ -7,36 +7,31 @@ ERRORS_NUMBER_CONFFILES=0
 #----------------------------#
 #  Directories 
 #----------------------------#
-if [[ ! -d /etc/apt/apt.conf.d/ ]]; then
-  ((ERRORS_NUMBER_DIR = ERRORS_NUMBER_DIR + 1))
-  echo "/etc/apt/apt.conf.d/ not a directory"
-fi
-if [[ ! -d ~/debian-setup/etc/apt/apt.conf.d/ ]]; then
-  ((ERRORS_NUMBER_DIR = ERRORS_NUMBER_DIR + 1))
-  echo "~/debian-setup/etc/apt/apt.conf.d/ not a directory"
-fi
+Directories[0]="/etc/apt/apt.conf.d/"
+Directories+=("/usr/local/bin/")
+Directories+=("~/debian-setup/etc/apt/apt.conf.d/")
+Directories+=("~/debian-setup/usr/local/bin/")
+for i in "${Directories[@]}"; do
+  if [[ ! -d $i ]]; then
+    ((ERRORS_NUMBER_DIR = ERRORS_NUMBER_DIR + 1))
+    echo "$i not a directory"
+  fi
+done
 
 #----------------------------#
 #  Symlinks
 #----------------------------#
-if [[ ! -h /etc/apt/apt.conf.d/50unattended-upgrades ]]; then
-  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
-  echo "/etc/apt/apt.conf.d/50unattended-upgrades not a symlink" 
-fi
-if [[ ! -h /etc/apt/apt.conf.d/20auto-upgrades ]]; then
-  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
-  echo "/etc/apt/apt.conf.d/20auto-upgrades not a symlink" 
-fi
-if [[ ! -h /etc/apt/apt.conf.d/100update ]]; then
-  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
-  echo "/etc/apt/apt.conf.d/100update not a symlink"
-  echo "Issue command 'ln -s ~/debian-setup/etc/apt/apt.conf.d/100update /etc/apt/apt.conf.d/100update'"
-fi
-if [[ ! -h ~/.bash_aliases ]]; then
-  ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
-  echo "~/.bash_aliases not a symlink." 
-  echo "Issue command 'ln -s ~/debian-setup/.bash_aliases ~/.bash_aliases'" 
-fi
+Symlinks[0]="/etc/apt/apt.conf.d/50unattended-upgrades"
+Symlinks+=("/etc/apt/apt.conf.d/20auto-upgrades")
+Symlinks+=("/etc/apt/apt.conf.d/100update")
+Symlinks+=("~/.bash_aliases")
+for i in "${Symlinks[@]}"; do
+  if [[ ! -h $i ]]; then
+    ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
+    echo "$i not a symlink."
+    echo "Please issue command 'ln -s ~/debian-setup$i $i'"
+  fi
+done
 
 #----------------------------#
 #  Configuration files
