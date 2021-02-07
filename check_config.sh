cd8d23838d2b2e84e85450c0c082778cd787d8ad
Thomas Huber <thomas.ch.huber@gmail.com>
2021-02-07 15:56:19 +0100

    check_config.sh Update

--- check_config.sh
+++ check_config.sh
@@ -7,10 +7,10 @@ ERRORS_NUMBER_CONFFILES=0
 #----------------------------#
 #  Directories 
 #----------------------------#
-Directories[0]="/etc/apt/apt.conf.d/"
-Directories+=("/usr/local/bin/")
-Directories+=("~/debian-setup/etc/apt/apt.conf.d/")
-Directories+=("~/debian-setup/usr/local/bin/")
+Directories[0]=/etc/apt/apt.conf.d/
+Directories+=(/usr/local/bin/)
+Directories+=(~/debian-setup/etc/apt/apt.conf.d/)
+Directories+=(~/debian-setup/usr/local/bin/)
 for i in "${Directories[@]}"; do
   if [[ ! -d $i ]]; then
     ((ERRORS_NUMBER_DIR = ERRORS_NUMBER_DIR + 1))
@@ -21,10 +21,10 @@ done
 #----------------------------#
 #  Symlinks
 #----------------------------#
-Symlinks[0]="/etc/apt/apt.conf.d/50unattended-upgrades"
-Symlinks+=("/etc/apt/apt.conf.d/20auto-upgrades")
-Symlinks+=("/etc/apt/apt.conf.d/100update")
-Symlinks+=("~/.bash_aliases")
+Symlinks[0]=/etc/apt/apt.conf.d/50unattended-upgrades
+Symlinks+=(/etc/apt/apt.conf.d/20auto-upgrades)
+Symlinks+=(/etc/apt/apt.conf.d/100update)
+Symlinks+=(~/.bash_aliases)
 for i in "${Symlinks[@]}"; do
   if [[ ! -h $i ]]; then
     ((ERRORS_NUMBER_SYMLINKS = ERRORS_NUMBER_SYMLINKS + 1))
@@ -36,22 +36,16 @@ done
 #----------------------------#
 #  Configuration files
 #----------------------------#
-if [[ ! -s ~/debian-setup/etc/apt/apt.conf.d/50unattended-upgrades ]]; then
-  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
-  echo "~/debian-setup/etc/apt/apt.conf.d/50unattended-upgrades does not exist or is empty" 
-fi
-if [[ ! -s ~/debian-setup/etc/apt/apt.conf.d/20auto-upgrades ]]; then
-  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
-  echo "~/debian-setup/etc/apt/apt.conf.d/20auto-upgrades does not exist or is empty" 
-fi
-if [[ ! -s ~/debian-setup/etc/apt/apt.conf.d/100update ]]; then
-  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
-  echo "~/debian-setup/etc/apt/apt.conf.d/100update does not exist or is empty" 
-fi
-if [[ ! -s ~/debian-setup/.bash_aliases ]]; then
-  ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
-  echo "~/debian-setup/.bash_aliases does not exist or is empty" 
-fi
+Conffiles[0]=~/debian-setup/etc/apt/apt.conf.d/50unattended-upgrades
+Conffiles+=(~/debian-setup/etc/apt/apt.conf.d/20auto-upgrades)
+Conffiles+=(~/debian-setup/etc/apt/apt.conf.d/100update)
+Conffiles+=(~/debian-setup/.bash_aliases)
+for i in "${Conffiles[@]}"; do
+  if [[ ! -s $i ]]; then
+    ((ERRORS_NUMBER_CONFFILES = ERRORS_NUMBER_CONFFILES + 1))
+    echo "$i does not exist or is empty" 
+  fi
+done
 
 if [[ ERRORS_NUMBER_DIR -eq 0 && 
       ERRORS_NUMBER_SYMLINKS -eq 0 &&
